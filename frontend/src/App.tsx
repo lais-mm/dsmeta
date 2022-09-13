67c019471fe2648c43cab58fe48642c769abdc13
Laís Moreira <lais-mm@outlook.com>
2022-09-13 19:44:46 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import { Fragment } from "react"
+import NotificationButton from "./componentes/NotificationButton"
+
 function App() {
- return (
-  <h1>olá mundo</h1>
- )
+    return (
+        <Fragment>
+
+            <h1>olá mundo</h1>
+            <NotificationButton />
+        </Fragment>
+
+    )
 }
 
 export default App
